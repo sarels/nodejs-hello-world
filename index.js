fd05eeca860d5c57f6885ebcbde4f7199be1f459
EC2 Default User <ec2-user@ip-172-31-10-221.eu-central-1.compute.internal>
2022-10-05 08:47:54 +0000

    test webhook #2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("test webhook #1 - Greetings Sarel Sadan!");
+    response.end("test webhook #2 - Greetings Sarel Sadan!");
 
 });
 
